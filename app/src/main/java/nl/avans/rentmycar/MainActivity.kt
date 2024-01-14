f85df1dec4160cfb6710dafe3942df7c0aa150ae
daanvangeloven <daanvangeloven@gmail.com>
2024-01-14 17:34:38 +0100

    Updated user settings page

--- app/src/main/java/nl/avans/rentmycar/MainActivity.kt
+++ app/src/main/java/nl/avans/rentmycar/MainActivity.kt
@@ -4,17 +4,25 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.*
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.auth0.android.Auth0
 import com.auth0.android.authentication.AuthenticationException
 import com.auth0.android.provider.WebAuthProvider
@@ -37,7 +45,6 @@ class MainActivity : ComponentActivity() {
     private var cachedUserProfile: UserProfile? = null
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -53,7 +60,7 @@ class MainActivity : ComponentActivity() {
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                if(cachedCredentials == null) {
+                if (cachedCredentials == null) {
                     LoginScreen()
                 } else {
                     AppNavigation()
@@ -62,113 +69,131 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-//    Login Screen with buttons
+    //    Login Screen with buttons
     @Preview
     @Composable
     fun LoginScreen() {
         Column(
-            modifier = Modifier
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = "Rent My Car",
-                style = MaterialTheme.typography.headlineLarge
-            )
+            modifier = with(Modifier) {
+                fillMaxSize()
+                    .paint(
+                        painterResource(id = R.drawable.background_road),
+                        contentScale = ContentScale.FillBounds,
+                    )},
+                verticalArrangement = Arrangement.Bottom,
+                horizontalAlignment = Alignment.CenterHorizontally
+                ) {
 
-            Button(onClick = { login() }) {
-                Text(text = "Login")
+                Text(
+                    text = "RENT MY CAR",
+                    style = MaterialTheme.typography.headlineLarge,
+                    fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
+                    color = androidx.compose.ui.graphics.Color.White,
+                )
+                Text(
+                    text = "Let's share a ride",
+                    style = MaterialTheme.typography.headlineSmall,
+                    color = androidx.compose.ui.graphics.Color.White,
+                    modifier = Modifier.padding(bottom = 32.dp)
+                )
+
+                Button(onClick = { login() }
+                , modifier = with(Modifier) {
+                    width(200.dp)
+                    padding(bottom = 16.dp)
+                    }) {
+                    Text(text = "Login")
+                }
+
+                if (cachedUserProfile != null) {
+                    Text(text = "User: ${cachedUserProfile?.name}")
+                }
             }
-            Button(onClick = { logout() }) {
-                Text(text = "Logout")
             }
 
-            if (cachedUserProfile != null) {
-                Text(text = "User: ${cachedUserProfile?.name}")
+                    private fun showUserProfile() {
+                // Guard against showing the profile when no user is logged in
+                if (cachedCredentials == null) {
+                    return
+                }
+
+                val client = AuthenticationAPIClient(account)
+                client
+                    .userInfo(cachedCredentials!!.accessToken!!)
+                    .start(object : Callback<UserProfile, AuthenticationException> {
+
+                        override fun onFailure(exception: AuthenticationException) {
+                            showSnackBar(
+                                getString(
+                                    R.string.general_failure_with_exception_code,
+                                    exception.getCode()
+                                )
+                            )
+                        }
+
+                        override fun onSuccess(profile: UserProfile) {
+                            cachedUserProfile = profile
+                            val userid = profile.getExtraInfo()["sub"].toString()
+                            saveUserId(userid)
+                            Log.d("UserID", "UserID: ${userid}")
+
+                        }
+
+                    })
             }
-        }
+
+                    private fun login() {
+                WebAuthProvider
+                    .login(account)
+                    .withScheme(getString(R.string.com_auth0_scheme))
+                    .withScope(getString(R.string.login_scopes))
+                    .withAudience(
+                        getString(
+                            R.string.login_audience,
+                            getString(R.string.com_auth0_domain)
+                        )
+                    )
+                    .start(this, object : Callback<Credentials, AuthenticationException> {
+
+                        override fun onFailure(exception: AuthenticationException) {
+                            showSnackBar(
+                                getString(
+                                    R.string.login_failure_message,
+                                    exception.getCode()
+                                )
+                            )
+                        }
+
+                        override fun onSuccess(credentials: Credentials) {
+                            cachedCredentials = credentials
+                            showUserProfile()
+                            Log.d(
+                                "UserID",
+                                "UserID: 2 2 2 ${
+                                    getPreferences(MODE_PRIVATE).getString(
+                                        "user_id",
+                                        null
+                                    )
+                                }"
+                            )
+                            setContent {
+                                AppNavigation()
+                            }
+                        }
+                    })
+            }
+
+                    private fun showSnackBar(text: String) {
+                Snackbar.make(findViewById(android.R.id.content), text, Snackbar.LENGTH_SHORT)
+                    .show()
+            }
+
+                    private fun saveUserId(userId: String) {
+                val sharedPreferences = getPreferences(MODE_PRIVATE)
+                val editor = sharedPreferences.edit()
+                editor.putString("user_id", userId)
+                editor.apply()
+            }
+
+
     }
-
-    private fun showUserProfile() {
-        // Guard against showing the profile when no user is logged in
-        if (cachedCredentials == null) {
-            return
-        }
-
-        val client = AuthenticationAPIClient(account)
-        client
-            .userInfo(cachedCredentials!!.accessToken!!)
-            .start(object : Callback<UserProfile, AuthenticationException> {
-
-                override fun onFailure(exception: AuthenticationException) {
-                    showSnackBar(getString(R.string.general_failure_with_exception_code,
-                        exception.getCode()))
-                }
-
-                override fun onSuccess(profile: UserProfile) {
-                    cachedUserProfile = profile
-                    val userid = profile.getExtraInfo()["sub"].toString()
-                    saveUserId(userid)
-                    Log.d("UserID", "UserID: ${userid}")
-
-                }
-
-            })
-    }
-
-    private fun login() {
-        WebAuthProvider
-            .login(account)
-            .withScheme(getString(R.string.com_auth0_scheme))
-            .withScope(getString(R.string.login_scopes))
-            .withAudience(getString(R.string.login_audience, getString(R.string.com_auth0_domain)))
-            .start(this, object : Callback<Credentials, AuthenticationException> {
-
-                override fun onFailure(exception: AuthenticationException) {
-                    showSnackBar(getString(R.string.login_failure_message, exception.getCode()))
-                }
-
-                override fun onSuccess(credentials: Credentials) {
-                    cachedCredentials = credentials
-                    showUserProfile()
-                    Log.d("UserID", "UserID: 2 2 2 ${getPreferences(MODE_PRIVATE).getString("user_id", null)}" )
-                    setContent {
-                        AppNavigation()
-                    }
-                }
-            })
-    }
-
-    private fun logout() {
-        WebAuthProvider
-            .logout(account)
-            .withScheme(getString(R.string.com_auth0_scheme))
-            .start(this, object : Callback<Void?, AuthenticationException> {
-
-                override fun onFailure(exception: AuthenticationException) {
-                    showSnackBar(getString(R.string.general_failure_with_exception_code,
-                        exception.getCode()))
-                }
-
-                override fun onSuccess(payload: Void?) {
-                    cachedCredentials = null
-                    cachedUserProfile = null
-                }
-
-            })
-    }
-
-    private fun showSnackBar(text: String) {
-        Snackbar.make(findViewById(android.R.id.content), text, Snackbar.LENGTH_SHORT).show()
-    }
-
-    private fun saveUserId(userId: String) {
-        val sharedPreferences = getPreferences(MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        editor.putString("user_id", userId)
-        editor.apply()
-    }
-
-
-}
--- app/src/main/java/nl/avans/rentmycar/ui/screens/SettingsScreen.kt
+++ app/src/main/java/nl/avans/rentmycar/ui/screens/SettingsScreen.kt
@@ -1,19 +1,30 @@
 package nl.avans.rentmycar.ui.screens
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Text
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import nl.avans.rentmycar.data.factory.UserViewModelFactory
-import nl.avans.rentmycar.ui.viewmodels.UserViewModel
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-
-
+import nl.avans.rentmycar.R
+import nl.avans.rentmycar.data.factory.UserViewModelFactory
+import nl.avans.rentmycar.data.model.User
+import nl.avans.rentmycar.ui.viewmodels.UserViewModel
 
 @Composable
 fun SettingsScreen(
@@ -21,36 +32,134 @@ fun SettingsScreen(
         factory = UserViewModelFactory()
     )
 ) {
-        val uiState by userViewModel.uiState.collectAsState()
-        val user = uiState.user
+    val uiState by userViewModel.uiState.collectAsState()
+    val user = uiState.user
 
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        if(user == null) {
-            Text(text = "No user logged in")
+        if (user == null) {
+            Text(text = stringResource(id = R.string.no_user_logged_in))
         } else {
-//            List of user data
-            LazyColumn() {
-                item {
-                    Text(text = "Name: ${user.name}")
-                }
-                item {
-                    Text(text = "Email: ${user.email}")
-                }
-                item {
-                    Text(text = "Address: ${user.address}")
-                }
-                item {
-                    Text(text = "City: ${user.city}")
-                }
-                item {
-                    Text(text = "Zipcode: ${user.zipcode}")
-                }
-
-            }
+            UserInfoSection(
+                user = user,
+                onNameChange = { /* Handle name change */ },
+                onEmailChange = { /* Handle email change */ },
+                onAddressChange = { /* Handle address change */ },
+                onCityChange = { /* Handle city change */ },
+                onZipcodeChange = { /* Handle zipcode change */ }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            EditButton(
+                text = stringResource(id = R.string.update_user_info),
+                icon = Icons.Default.ExitToApp,
+                onClick = { /* Handle update user info click */ }
+            )
+            Spacer(modifier = Modifier.height(30.dp))
+            LogoutButton(onLogoutClick = { /* Handle logout */ })
         }
-
     }
 }
+
+@Composable
+private fun UserInfoSection(
+    user: User,
+    onNameChange: (String) -> Unit,
+    onEmailChange: (String) -> Unit,
+    onAddressChange: (String) -> Unit,
+    onCityChange: (String) -> Unit,
+    onZipcodeChange: (String) -> Unit
+) {
+    LazyColumn() {
+        item {
+            EditableTextField(
+                label = stringResource(id = R.string.edit_name),
+                value = user.name,
+                onValueChange = onNameChange
+            )
+        }
+        item {
+            EditableTextField(
+                label = stringResource(id = R.string.edit_email),
+                value = user.email,
+                onValueChange = onEmailChange
+            )
+        }
+        item {
+            EditableTextField(
+                label = stringResource(id = R.string.edit_address),
+                value = user.address,
+                onValueChange = onAddressChange
+            )
+        }
+        item {
+            EditableTextField(
+                label = stringResource(id = R.string.edit_city),
+                value = user.city,
+                onValueChange = onCityChange
+            )
+        }
+        item {
+            EditableTextField(
+                label = stringResource(id = R.string.edit_zipcode),
+                value = user.zipcode,
+                onValueChange = onZipcodeChange
+            )
+        }
+    }
+}
+
+@Composable
+private fun EditableTextField(label: String, value: String, onValueChange: (String) -> Unit) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = { onValueChange(it) },
+        label = { Text(label) },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    )
+}
+
+@Composable
+private fun EditButton(text: String, icon: ImageVector, onClick: () -> Unit) {
+    val buttonColor = Color(android.graphics.Color.parseColor("#82DD55")) // Replace with your desired hex color code
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(buttonColor, CircleShape)
+            .clickable { onClick() }
+            .padding(bottom = 8.dp, top = 8.dp, start = 16.dp, end = 16.dp)
+        ,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = text, color = Color.White, modifier = Modifier.weight(1f))
+        Icon(imageVector = icon, contentDescription = null, tint = Color.White)
+    }
+}
+
+@Composable
+private fun LogoutButton(onLogoutClick: () -> Unit) {
+    Button(
+        onClick = { onLogoutClick() },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        colors = ButtonDefaults.textButtonColors(contentColor = Color.Red)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Icon(imageVector = Icons.Default.ExitToApp, contentDescription = null)
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = stringResource(id = R.string.logout), color = Color.Red)
+        }
+    }
+}
+
