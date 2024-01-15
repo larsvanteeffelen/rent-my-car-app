08aa58d66d37036bdfdca8c1a168076cf2492d19
Lars Teeffelen <lars.teeffelen@gmail.com>
2024-01-15 19:13:27 +0100

    Changed HttpClient to singleton

--- app/src/main/java/nl/avans/rentmycar/data/api/CarApi.kt
+++ app/src/main/java/nl/avans/rentmycar/data/api/CarApi.kt
@@ -5,13 +5,12 @@ import io.ktor.client.statement.bodyAsText
 import io.ktor.http.isSuccess
 import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.Json
-import nl.avans.rentmycar.client.getBaseURL
-import nl.avans.rentmycar.client.getClient
+import nl.avans.rentmycar.client.HttpClient
 import nl.avans.rentmycar.data.model.Car
 
 class CarApi {
 
-    private val client = getClient()
+    private val client = HttpClient.client
     suspend fun fetchCars(): List<Car> {
 
         val response = client.get("http://10.0.2.2:8080/car/all")
